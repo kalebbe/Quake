cf30876486f744b37002054923a765c7eb5e77c0
Kaleb <kalebeberhart10@gmail.com>
2019-09-28 16:55:32 -0700

    Initial version of Quake IoT
    This is the first version of the IoT application for our cst-361.

---
+++ Quake/Quake/Earthquake.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * This is the object class for the earthquakes being 
+ * consumed by the API.
+ * 
+ * @authors Kaleb Eberhart, Mick Torres
+ * @version 1.0
+ * @since   2019-09-28
+ */
+
+using System;
+
+namespace Quake
+{
+    class Earthquake
+    {
+        private double magnitude;
+        private DateTime dateTime;
+        private double depth;
+        private String location;
+        private String coordinates;
+
+        //Constructor for the earthquake class to instantiate earthquakes.
+        public Earthquake(double magnitude, DateTime dateTime, double depth, 
+            String location, String coordinates)
+        {
+            this.magnitude = magnitude;
+            this.dateTime = dateTime;
+            this.depth = depth;
+            this.location = location;
+            this.coordinates = coordinates;
+        }
+
+        //Getters and setters for all of the variables in the Earthquake class
+        public double Magnitude { get => magnitude; set => magnitude = value; }
+        public DateTime Date { get => dateTime; set => dateTime = value; }
+        public double Depth { get => depth; set => depth = value; }
+        public String Location { get => location; set => location = value; }
+        public String Coordinates { get => coordinates; set => coordinates = value; }
+    }
+}
---
+++ Quake/Quake/Program.cs
@@ -0,0 +1,72 @@
+﻿/**
+ * This is the driving class that consumes an API from the
+ * earthquake website to retrieve earthquake data. In future
+ * milestones, this IoT app will create objects out of each
+ * consumed earthquake to return them to the reporting website.
+ * 
+ * @authors Kaleb Eberhart, Mick Torres
+ * @version 1.0
+ * @since   2019-09-28
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace Quake
+{
+    class Program
+    {
+        //To be used for parsing earthquakes into objects later on
+        private List<Earthquake> earthquakes;
+
+        //Query to get json data from earthquake website. Defaults to past 30 days.
+        private const string url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson";
+
+        /**
+         * This is the main method for this IoT application.
+         * 
+         * @param string[]
+         * @return void
+         */
+        static void Main(string[] args)
+        {
+            //This sends a request to the API with the desired parameters.
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                //This is the response from the server
+                WebResponse response = request.GetResponse();
+
+                //We're currently using stream and streamreader to retrieve the data
+                //then parse it into json. This is a jerry rigged way to do this and
+                //will also be fixed in a future milestone. The response should be
+                //written in geojson already, so this parsing is unecessary.
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    JObject parsed = JObject.Parse(reader.ReadToEnd());
+
+                    foreach(var p in parsed)
+                    {
+                        Console.WriteLine("{0}: {1}", p.Key, p.Value);
+                    }
+                }
+            }
+            //Catching a negative response from the server.
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream,Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                }
+                throw;
+            }
+        }
+    }
+}
